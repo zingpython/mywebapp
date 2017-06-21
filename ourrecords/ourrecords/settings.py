4b8c941614324a561b3dd59ba3b7c01070265871
McLaren <artyudin@outlook.com>
2017-06-20 23:51:56 -0400

    settings

--- ourrecords/ourrecords/settings.py
+++ ourrecords/ourrecords/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'       
-STATIC_ROOT = '/var/www/venv/mywebapp/ourrecords/static/'
+STATIC_ROOT = '/var/www/env/mywebapp/ourrecords/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/var/www/venv/mywebapp/ourrecords/staticfiles/')
+    os.path.join(BASE_DIR, '/var/www/env/mywebapp/ourrecords/staticfiles/')
 ]
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/venv/mywebapp/ourrecords/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/env/mywebapp/ourrecords/media/')
